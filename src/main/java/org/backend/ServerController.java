80820c84d9626f9611776ca1707ece1a2e421e69
gogo1414 <wnstn6945@gmail.com>
2024-07-24 16:44:56 +0900

    ServerController 상세 설명 작성

--- src/main/java/org/backend/ServerController.java
+++ src/main/java/org/backend/ServerController.java
@@ -1,13 +1,17 @@
 package org.backend;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Server Controller", description = "서버 작동 여부 확인")
 public class ServerController {
 
     @GetMapping("/hello")
+    @Operation(summary = "서버 동작 확인", description = "서버 동작을 확인하기 위해 사용하는 API")
     public String hello(Model model) {
         return "Hello, World.";
     }
