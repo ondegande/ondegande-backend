a9dd51c8c41604d13c746edb71290427b5c22f47
gogo1414 <wnstn6945@gmail.com>
2024-07-24 16:42:41 +0900

    Location API 상세 설명 작성

--- src/main/java/org/backend/global/config/SwaggerConfig.java
+++ src/main/java/org/backend/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,14 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("Authorization", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(apiInfo());
     }
 
@@ -20,6 +28,6 @@ public class SwaggerConfig {
         return new Info()
                 .title("부산's 온데간데 Test API")
                 .description("사용자의 여행 코스 및 관광지에 관한 API")
-                .version("1.0.0");
+                .version("1.0.1");
     }
 }
