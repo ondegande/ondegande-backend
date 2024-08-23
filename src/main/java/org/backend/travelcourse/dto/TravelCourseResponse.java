c750bbe4eabb3da4b61b09e89ca7050fad76dd02
gogo1414 <wnstn6945@gmail.com>
2024-08-23 21:55:00 +0900

    refactor: Cors 설정 변경 및 여행코스 컬럼 수정

--- src/main/java/org/backend/travelcourse/dto/TravelCourseResponse.java
+++ src/main/java/org/backend/travelcourse/dto/TravelCourseResponse.java
@@ -1,23 +1,16 @@
 package org.backend.travelcourse.dto;
 
-import org.backend.travelcourse.domain.Accommodation;
-import org.backend.travelcourse.domain.Concept;
-import org.backend.travelcourse.domain.Schedule;
 import org.backend.travelcourse.domain.TravelCourse;
 
 public record TravelCourseResponse(
         Long id,
-        Schedule schedule,
-        Concept concept,
-        Accommodation accommodation,
+        String name,
         boolean isShared
 ) {
     public static TravelCourseResponse toResponseDto(TravelCourse travelCourse) {
         return new TravelCourseResponse(
                 travelCourse.getId(),
-                travelCourse.getSchedule(),
-                travelCourse.getConcept(),
-                travelCourse.getAccommodation(),
+                travelCourse.getName(),
                 travelCourse.isShared()
         );
     }
