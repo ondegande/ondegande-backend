023ef6c85a3cbb30b661541cc638126793934052
gogo1414 <wnstn6945@gmail.com>
2024-08-23 21:36:41 +0900

    refactor: cors 설정 변경 및 travelcourse 도메인 변경 - cors localhost:3000 및 ec2 IP 추가 - travelcourse 도메인 요구사항에 맞게 수정

--- src/main/java/org/backend/travelcourse/dto/TravelCourseRequest.java
+++ src/main/java/org/backend/travelcourse/dto/TravelCourseRequest.java
@@ -1,22 +1,12 @@
 package org.backend.travelcourse.dto;
 
-import org.backend.courseinfo.Accommodation;
-import org.backend.courseinfo.Concept;
-import org.backend.courseinfo.Schedule;
 import org.backend.travelcourse.domain.TravelCourse;
 
-public record TravelCourseRequest(
-        Schedule schedule,
-        Concept concept,
-        Accommodation accommodation,
-        boolean isShared
-) {
+public record TravelCourseRequest(String name, boolean isShared) {
 
     public TravelCourse toEntity() {
         return new TravelCourse(
-                schedule,
-                concept,
-                accommodation,
+                name,
                 isShared
         );
     }
