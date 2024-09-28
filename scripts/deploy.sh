a2c696e890c10829a5ea09ca48402da06d493b49
gogo1414 <wnstn6945@gmail.com>
2024-09-28 17:07:32 +0900

    hotfix: logback 적용을 위한 배포 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,4 +30,5 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-prod.yml,classpath:/application-oauth.yml \
+  -Dlogging.config=$REPOSITORY/logback.xml \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
