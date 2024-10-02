9b9e2f7a62593317388876ae920ce47ebb7deb41
gogo1414 <wnstn6945@gmail.com>
2024-10-02 22:05:41 +0900

    fix: 배포 스크립트 오타 수정 및 실행 권한 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ CURRENT_PID=$(pgrep -f ${REPOSITORY}.*.jar)
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -41,7 +41,7 @@ echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
