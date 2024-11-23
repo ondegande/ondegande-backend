0da0a4d7d6c29bedc0b5ef781c42d752280f7a15
gogo1414 <wnstn6945@gmail.com>
2024-11-24 00:39:50 +0900

    refactor: redis 비밀번호 설정 제거

--- src/main/java/org/backend/global/config/RedisCacheConfig.java
+++ src/main/java/org/backend/global/config/RedisCacheConfig.java
@@ -16,11 +16,8 @@ public class RedisCacheConfig {
     @Bean
     public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                // 캐시 만료 시간 설정 (예: 1시간)
                 .entryTtl(Duration.ofHours(1))
-                // null 값 캐시 안 함
                 .disableCachingNullValues()
-                // 직렬화 설정 (키와 값을 문자열로 직렬화)
                 .serializeKeysWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
--- src/main/java/org/backend/global/config/RedisConfig.java
+++ src/main/java/org/backend/global/config/RedisConfig.java
@@ -16,19 +16,11 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int REDIS_PORT;
 
-    // 로컬에서만 해당
-//    @Value("${spring.data.redis.password}")
-//    private String REDIS_PASSWORD;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-
         configuration.setHostName(REDIS_HOST);
         configuration.setPort(REDIS_PORT);
-//        configuration.setPassword(REDIS_PASSWORD);
-
         return new LettuceConnectionFactory(configuration);
     }
-
 }
